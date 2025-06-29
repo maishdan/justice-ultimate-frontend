5c4e263f3a2b0132cc047f9803e2bc9e950c3c58
maishdan <maishdan4940@gmail.com>
2025-06-29 05:27:33 +0300

    fix: use @tailwindcss/postcss and update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+// postcss.config.js
+
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss')({
+      config: './tailwind.config.js',
+    }),
+    require('autoprefixer'),
+  ],
 };
